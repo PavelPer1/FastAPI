631e04724eeb97b3badee8dba96747384be36f75
PavelPer1 <lolhyh456@gmail.com>
2026-02-18 19:27:40 +0300

    init

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
+from authx import AuthX, AuthXConfig
+from fastapi.openapi.models import Response
+from pydantic import BaseModel
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+security = AuthX(config=config)
 
+class Base(BaseModel):
+    pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class UserLoginScheme(Base):
+    username: str
+    password: str
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/login")
+def login(cred: UserLoginScheme, response: Response):
+    if cred.username == "test" and cred.password == "test":
+        token = security.create_access_token(uid="1234")
+        return {"access_token": token}
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+@app.get("/protected")
+def protected():
+    ...
+
+if __name__ == "__main__":
+    uvicorn.run(app)
